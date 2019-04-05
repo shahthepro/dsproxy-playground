98e61ad503f289703b6f0e11f852315b59ee94d6
Shah <shahthepro@outlook.com>
2019-04-05 20:20:46 +0530

    Change transfer to transferFrom

--- src/DsproxyPlayground.sol
+++ src/DsproxyPlayground.sol
@@ -4,12 +4,13 @@ import "ds-math/math.sol";
 
 interface IToken {
     function transfer(address, uint) external returns (bool);
+    function transferFrom(address, address, uint) external returns (bool);
 }
 
 contract DsproxyPlayground is DSMath {
     function delegateTransfer(address _token, uint256 _value) public {
         IToken token = IToken(_token);
 
-        token.transfer(address(0), _value);
+        require(token.transferFrom(msg.sender, address(0), _value));
     }
 }
