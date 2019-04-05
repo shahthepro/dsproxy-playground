952cd44687878f4cc00a3228fbce5cf279e6255f
Shah <shahthepro@outlook.com>
2019-04-05 09:12:23 +0530

    Add delegateTransfer() method

--- src/DsproxyPlayground.sol
+++ src/DsproxyPlayground.sol
@@ -1,4 +1,15 @@
 pragma solidity ^0.5.6;
 
-contract DsproxyPlayground {
+import "ds-math/math.sol";
+
+interface IToken {
+    function transfer(address, uint) external returns (bool);
+}
+
+contract DsproxyPlayground is DSMath {
+    function delegateTransfer(address _token, uint256 _value) public {
+        IToken token = IToken(_token);
+
+        token.transfer(address(0), _value);
+    }
 }
